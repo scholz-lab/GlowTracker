23296b0f5da84a4d1bc9d4559fcc3a8c1a81d82d
Micro-Sandworms <luis.alvarez@caesar.de>
2020-12-01 17:00:56 +0100

    Corrected bug of the protocol connecting to the stage Removed some superfluous code Added a few comments

--- MacroscopeGUI.py
+++ MacroscopeGUI.py
@@ -24,18 +24,17 @@ from zaber_motion.ascii import Connection
 import MacroscopeStageScripts as stg
 import FunctioningImageUpdate as imgupdate  # This works with the BASLER to grab the images, but we are using now the webcam. The idea would be to grab images with this and the use kivy-garden for the display.
 
-Library.toggle_device_db_store(True)
-
+Library.toggle_device_db_store(True)  # This is part of the Zaber initialization. Unclear what it does.
 
 Window.size = (1600, 600)
-
+# MenuBarWidget is the main GUI
 class MenuBarWidget(Widget):
     def __init__(self, **kwargs):
         super(MenuBarWidget, self).__init__(**kwargs)
-        
+
     def home(self):
         stg.home_stage()
-    
+
     def exit_app(self):
         kivy.base.stopTouchApp()
 
@@ -43,13 +42,11 @@ class MenuBarWidget(Widget):
 class AxisLimitsPopup(Popup):
     def __init__(self, **kwargs):
         super(AxisLimitsPopup, self).__init__(**kwargs)
-    
     #TODO!: Add function that changes entered new limit in stage script fct set_rangelimits()
 
 class RecordingSettingsPopup(Popup):
     def __init__(self, **kwargs):
         super(RecordingSettingsPopup, self).__init__(**kwargs)
-
     #TODO!: Add functions looking up according strings in camera file and changing following values
     #(exposure time, gain)
     #to change number of frames to record: change while loop limit in stg.scripts
@@ -82,62 +79,34 @@ class XControls(Widget):
     
     def moveX_bigStepLeft(self):
         with Connection.open_serial_port('COM3') as connection:
+            connection.renumber_devices(first_address=1)
             device_list = connection.detect_devices()
-            connection.renumber_devices(first_address = 1)
-        
             device1 = device_list[0]
-            device2 = device_list[1]
             axis_x = device1.get_axis(1)
-            axis_y = device2.get_axis(1)
-            if len(device_list) > 2:
-                device3 = device_list[2]
-                axis_z = device3.get_axis(1)
-                
             axis_x.move_relative(-50, Units.LENGTH_MICROMETRES, wait_until_idle=False)
 
     def moveX_smallStepLeft(self):
         with Connection.open_serial_port('COM3') as connection:
+            connection.renumber_devices(first_address=1)
             device_list = connection.detect_devices()
-            connection.renumber_devices(first_address = 1)
-            
             device1 = device_list[0]
-            device2 = device_list[1]
             axis_x = device1.get_axis(1)
-            axis_y = device2.get_axis(1)
-            if len(device_list) > 2:
-                device3 = device_list[2]
-                axis_z = device3.get_axis(1)
-            
             axis_x.move_relative(-20, Units.LENGTH_MICROMETRES, wait_until_idle=False)
 
     def moveX_smallStepRight(self):
         with Connection.open_serial_port('COM3') as connection:
+            connection.renumber_devices(first_address=1)
             device_list = connection.detect_devices()
-            connection.renumber_devices(first_address = 1)
-            
             device1 = device_list[0]
-            device2 = device_list[1]
             axis_x = device1.get_axis(1)
-            axis_y = device2.get_axis(1)
-            if len(device_list) > 2:
-                device3 = device_list[2]
-                axis_z = device3.get_axis(1)
-            
             axis_x.move_relative(20, Units.LENGTH_MICROMETRES, wait_until_idle=False)
 
     def moveX_bigStepRight(self):
         with Connection.open_serial_port('COM3') as connection:
+            connection.renumber_devices(first_address=1)
             device_list = connection.detect_devices()
-            connection.renumber_devices(first_address = 1)
-            
             device1 = device_list[0]
-            device2 = device_list[1]
             axis_x = device1.get_axis(1)
-            axis_y = device2.get_axis(1)
-            if len(device_list) > 2:
-                device3 = device_list[2]
-                axis_z = device3.get_axis(1)
-            
             axis_x.move_relative(50, Units.LENGTH_MICROMETRES, wait_until_idle=False)
 
 class YControls(Widget):
@@ -146,62 +115,34 @@ class YControls(Widget):
 
     def moveY_bigStepLeft(self):
         with Connection.open_serial_port('COM3') as connection:
+            connection.renumber_devices(first_address=1)
             device_list = connection.detect_devices()
-            connection.renumber_devices(first_address = 1)
-        
-            device1 = device_list[0]
             device2 = device_list[1]
-            axis_x = device1.get_axis(1)
             axis_y = device2.get_axis(1)
-            if len(device_list) > 2:
-                device3 = device_list[2]
-                axis_z = device3.get_axis(1)
-                
             axis_y.move_relative(-50, Units.LENGTH_MICROMETRES, wait_until_idle=False)
 
     def moveY_smallStepLeft(self):
         with Connection.open_serial_port('COM3') as connection:
+            connection.renumber_devices(first_address=1)
             device_list = connection.detect_devices()
-            connection.renumber_devices(first_address = 1)
-            
-            device1 = device_list[0]
             device2 = device_list[1]
-            axis_x = device1.get_axis(1)
             axis_y = device2.get_axis(1)
-            if len(device_list) > 2:
-                device3 = device_list[2]
-                axis_z = device3.get_axis(1)
-            
             axis_y.move_relative(-20, Units.LENGTH_MICROMETRES, wait_until_idle=False)
 
     def moveY_smallStepRight(self):
         with Connection.open_serial_port('COM3') as connection:
+            connection.renumber_devices(first_address=1)
             device_list = connection.detect_devices()
-            connection.renumber_devices(first_address = 1)
-            
-            device1 = device_list[0]
             device2 = device_list[1]
-            axis_x = device1.get_axis(1)
             axis_y = device2.get_axis(1)
-            if len(device_list) > 2:
-                device3 = device_list[2]
-                axis_z = device3.get_axis(1)
-            
             axis_y.move_relative(20, Units.LENGTH_MICROMETRES, wait_until_idle=False)
 
     def moveY_bigStepRight(self):
         with Connection.open_serial_port('COM3') as connection:
+            connection.renumber_devices(first_address=1)
             device_list = connection.detect_devices()
-            connection.renumber_devices(first_address = 1)
-            
-            device1 = device_list[0]
             device2 = device_list[1]
-            axis_x = device1.get_axis(1)
             axis_y = device2.get_axis(1)
-            if len(device_list) > 2:
-                device3 = device_list[2]
-                axis_z = device3.get_axis(1)
-            
             axis_y.move_relative(50, Units.LENGTH_MICROMETRES, wait_until_idle=False)
 
 class ZControls(Widget):
@@ -210,63 +151,47 @@ class ZControls(Widget):
 
     def moveZ_bigStepLeft(self):
         with Connection.open_serial_port('COM3') as connection:
+            connection.renumber_devices(first_address=1)
             device_list = connection.detect_devices()
-            connection.renumber_devices(first_address = 1)
-        
-            device1 = device_list[0]
-            device2 = device_list[1]
-            axis_x = device1.get_axis(1)
-            axis_y = device2.get_axis(1)
             if len(device_list) > 2:
                 device3 = device_list[2]
                 axis_z = device3.get_axis(1)
-                
-            axis_z.move_relative(-50, Units.LENGTH_MICROMETRES, wait_until_idle=False)
+                axis_z.move_relative(-50, Units.LENGTH_MICROMETRES, wait_until_idle=False)
+            else:
+                print('No Z axis detected')
 
     def moveZ_smallStepLeft(self):
         with Connection.open_serial_port('COM3') as connection:
+            connection.renumber_devices(first_address=1)
             device_list = connection.detect_devices()
-            connection.renumber_devices(first_address = 1)
-            
-            device1 = device_list[0]
-            device2 = device_list[1]
-            axis_x = device1.get_axis(1)
-            axis_y = device2.get_axis(1)
             if len(device_list) > 2:
                 device3 = device_list[2]
                 axis_z = device3.get_axis(1)
-            
-            axis_z.move_relative(-20, Units.LENGTH_MICROMETRES, wait_until_idle=False)
+                axis_z.move_relative(-20, Units.LENGTH_MICROMETRES, wait_until_idle=False)
+            else:
+                print('No Z axis detected')
 
     def moveZ_smallStepRight(self):
         with Connection.open_serial_port('COM3') as connection:
+            connection.renumber_devices(first_address=1)
             device_list = connection.detect_devices()
-            connection.renumber_devices(first_address = 1)
-            
-            device1 = device_list[0]
-            device2 = device_list[1]
-            axis_x = device1.get_axis(1)
-            axis_y = device2.get_axis(1)
             if len(device_list) > 2:
                 device3 = device_list[2]
                 axis_z = device3.get_axis(1)
-            
-            axis_z.move_relative(20, Units.LENGTH_MICROMETRES, wait_until_idle=False)
+                axis_z.move_relative(20, Units.LENGTH_MICROMETRES, wait_until_idle=False)
+            else:
+                print('No Z axis detected')
 
     def moveZ_bigStepRight(self):
         with Connection.open_serial_port('COM3') as connection:
+            connection.renumber_devices(first_address=1)
             device_list = connection.detect_devices()
-            connection.renumber_devices(first_address = 1)
-            
-            device1 = device_list[0]
-            device2 = device_list[1]
-            axis_x = device1.get_axis(1)
-            axis_y = device2.get_axis(1)
             if len(device_list) > 2:
                 device3 = device_list[2]
                 axis_z = device3.get_axis(1)
-            
-            axis_z.move_relative(50, Units.LENGTH_MICROMETRES, wait_until_idle=False)
+                axis_z.move_relative(50, Units.LENGTH_MICROMETRES, wait_until_idle=False)
+            else:
+                print('No Z axis detected')
 
 class FunctionalBodyWidget(BoxLayout):
     def __init__(self, **kwargs):
@@ -277,11 +202,10 @@ class OverallMenu(GridLayout):
     def __init__(self, **kwargs):
         super(OverallMenu, self).__init__(**kwargs)
 
+
 class CameraWidget(BoxLayout):
     def __init__(self, **kwargs):
         super(CameraWidget, self).__init__(**kwargs)
-    
-    
 
 
 class MainBoxWidget(GridLayout):
@@ -290,11 +214,15 @@ class MainBoxWidget(GridLayout):
         
 
 class MacroscopeApp(App):
+    # This is used by kivy as the entry point for the definitions of the UI. It is standard. It needs to use the App as
+    # input and its name has to end with App. The actual "meat" of the widgets and what do they do and their layout is
+    # in another file that must have the same name but low case, without the app at the end and .kv extension
     def build(self):
-        return MainBoxWidget()
+        return MainBoxWidget()  # This is the actuall GUI
 
 
 def reset():
+    # Cleaner for the events in memory
     import kivy.core.window as window
     from kivy.base import EventLoop
     if not EventLoop.event_listeners:
