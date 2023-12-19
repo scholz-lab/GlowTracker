81399c99e7cd1b58ceb2e9e3fbabbd2d3fe33857
takkasila <takkasila@outlook.com>
2023-12-19 13:17:45 +0100

    Clear unused packages and add setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    name='glowtracker',
+    version='1.0',
+    description='GlowTracker is a macroscope tracking application that have a capability of tracking small animal in bright field, single or dual epi-fluorescence imaging.',
+    url='https://github.com/scholz-lab/GlowTracker',
+    author='Monika Scholz',
+    author_email='monika.scholz@mpinb.mpg.de',
+    packages=['glowtracker'],
+    install_requires=[
+        "kivy>=2.2.1",
+        "numpy>=1.25.1",
+        "matplotlib>=3.7.2",
+        "zaber-motion>=4.2.0",
+        "pypylon>=2.2.0",
+        "opencv-python>=4.7.0",
+        "scipy>=1.11.1",
+        "scikit-image>=0.21.0",
+        "itk-elastix",
+    ],
+    keywords=[
+        'python',
+        'research',
+        'macroscope',
+        'tracking',
+        'Zaber',
+        'Basler',
+    ],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python'
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+    ],
+)
