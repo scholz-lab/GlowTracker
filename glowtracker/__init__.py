478c31420be15a2b963e72b05d46af68c91cabcc
takkasila <takkasila@outlook.com>
2024-01-12 11:54:09 +0100

    Remove setup.py

--- glowtracker/__init__.py
+++ glowtracker/__init__.py
@@ -1,7 +1,3 @@
-__version__ = "1.0.0"
-__author__ = 'Monika Scholz'
-__credits__ = 'Euphrasie Ramahefarivo, Takkasila Saichol, Behzad Shomali, Luis Alvarez'
-
 # This acts as an entry point for a convenient executable script
 def initApp():
     from . import __main__
