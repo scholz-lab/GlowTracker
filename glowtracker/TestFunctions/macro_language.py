b74460e98fb8959cdc7b4410ebb1a5ddd8ac6d59
takkasila <takkasila@outlook.com>
2024-06-21 13:55:43 +0200

    Restructured and create command executor

--- glowtracker/TestFunctions/macro_language.py
+++ glowtracker/TestFunctions/macro_language.py
@@ -1,68 +1,140 @@
 from pyparsing import (
-    Word, alphas, alphanums, nums, delimitedList, Group, 
-    Suppress, Forward, Optional, oneOf, ZeroOrMore, infixNotation, opAssoc, Keyword, Combine, ParserElement, StringEnd, ParseException, common
+    Group, Suppress, Forward, ZeroOrMore, Keyword, ParserElement, StringEnd, ParseException, common
 )
 
-# Ignore space, tabs, return, newline
-ParserElement.setDefaultWhitespaceChars(' \t\r\n')
+def createTextParser() -> ParserElement:
 
-# Define arguments
-empty_arg = Suppress('()')
-int_arg = Suppress('(') + common.integer + Suppress(')')
-float_arg = Suppress('(') + common.real + Suppress(')')
-number_arg = Suppress('(') + common.number + Suppress(')')
-coord_args = Suppress('(') + delimitedList(common.number) + Suppress(')')
+    # 
+    # Define Grammar
+    # 
+    # Arguments
+    empty_arg = Suppress('()')
+    int_arg = Suppress('(') + common.integer + Suppress(')')
+    number_arg = Suppress('(') + common.number + Suppress(')')
+    coord_args = Suppress('(') + common.number + Suppress(',') + common.number + Suppress(',') + common.number + Suppress(')')
 
-# Define commands
-move_abs = Keyword('move_abs') + coord_args
-move_rel = Keyword('move_rel') + coord_args
-snap = Keyword('snap') + empty_arg
-record_for = Keyword('record_for') + number_arg
-start_recording = Keyword('start_recording') + empty_arg
-stop_recording = Keyword('stop_recording') + empty_arg
-wait = Keyword('wait') + number_arg
+    # Commands
+    move_abs = Group( Keyword('move_abs') + coord_args )
+    move_rel = Group( Keyword('move_rel') + coord_args )
+    snap = Group( Keyword('snap') + empty_arg )
+    record_for = Group( Keyword('record_for') + number_arg )
+    start_recording = Group( Keyword('start_recording') + empty_arg )
+    stop_recording = Group( Keyword('stop_recording') + empty_arg )
+    wait = Group( Keyword('wait') + number_arg )
 
-# Forward declaration for nested loops
-command = Forward()
+    # Forward declaration for nested loops
+    command = Forward()
 
-# Define loop command with nested commands
-loop = Keyword("loop") + int_arg + Suppress('{') + Group(ZeroOrMore(command)) + Suppress('}')
+    # Define loop command with nested commands
+    loop = Group( Keyword("loop") + int_arg + Suppress('{') + Group(ZeroOrMore(command)) + Suppress('}') )
 
-# Finally define command which included loop
-command <<= (
-    move_abs | move_rel | snap | record_for | start_recording |
-    stop_recording | wait | loop
-)
+    # Finally define command which included loop
+    command <<= (
+        move_abs | move_rel | snap | record_for | start_recording |
+        stop_recording | wait | loop
+    )
 
-# Top-level parser
-script = ZeroOrMore(command) + StringEnd()
+    # Top-level parser
+    parser = ZeroOrMore(command) + StringEnd()
 
-# Example script
-script_text = """
-move_abs(1.0, 2.0, 3.0)
-move_rel(0.5, -0.5, 0)
-snap()
-record_for(3.5)
-start_recording()
-wait(2.0)
-loop(5) {
-    move_abs(1.1, 2.1, 3.1)
-    wait(1.0)
-    loop(3) {
-        snap()
-        wait(0.5)
+    # Ignore space, tabs, return, newline
+    parser.setDefaultWhitespaceChars(' \t\r\n')
+
+    return parser
+
+
+def getMacroScript() -> str:
+    # Example script
+    script_text = """
+    move_abs(1.0, 2.0, 3.0)
+    move_rel(0.5, -0.5, 0)
+    snap()
+    record_for(3.5)
+    start_recording()
+    wait(2.0)
+    loop(5) {
+        move_abs(1.1, 2.1, 3.1)
+        wait(1.0)
+        loop(3) {
+            snap()
+            wait(0.5)
+        }
     }
-}
-stop_recording()
-"""
+    stop_recording()
+    """
 
-try:
-    # Parse the script
-    parsed_script = script.parseString(script_text, parseAll= True)
+    return script_text
 
-    # Print parsed result
-    for cmd in parsed_script:
-        print(cmd)
 
-except ParseException as pe:
-    print(f"Parsing error: {pe}")
+def executeCommandList(commandList) -> None:
+    
+    for command in commandList:
+        
+        commandName = command[0]
+
+        if commandName == 'move_abs':
+
+            [x, y, z] = command[1:4]
+            print(f'Move absolute for {x, y, z}')
+            
+        elif commandName == 'move_rel':
+
+            [x, y, z] = command[1:4]
+            print(f'Move relative for {x, y, z}')
+            
+        elif commandName == 'snap':
+            
+            print('Snap an image')
+            
+        elif commandName == 'record_for':
+            
+            recordTime = command[1]
+            print(f'Record for {recordTime} sec')
+            
+        elif commandName == 'start_recording':
+
+            print('Start recording')
+            
+        elif commandName == 'stop_recording':
+            
+            print('Stop recording')
+
+        elif commandName == 'wait':
+
+            waitTime = command[1]
+            print(f'Wait for {waitTime} sec')
+        
+        elif commandName == 'loop':
+
+            numLoop = command[1]
+            subCommandList = command[2]
+
+            for i in range(numLoop):
+                
+                print(f'Loop {i}')
+                executeCommandList(subCommandList)
+
+
+
+if __name__ == '__main__':
+
+    macroParser = createTextParser()
+
+    macroScript = getMacroScript()
+
+    parsedCommands = []
+
+    try:
+        # Parse the script
+        parsedCommands = macroParser.parseString(macroScript, parseAll= True)
+
+    except ParseException as pe:
+        print(f"Parsing error: {pe}")
+        exit
+    
+    print('Parsed command successful.')
+
+    for command in parsedCommands:
+        print(command)
+
+    executeCommandList(parsedCommands)
