323c8eecec2ebfce90464b786892846a572baf11
takkasila <takkasila@outlook.com>
2024-06-25 13:42:37 +0200

    Start adding support for custom variable

--- glowtracker/TestFunctions/macro_language.py
+++ glowtracker/TestFunctions/macro_language.py
@@ -1,6 +1,6 @@
 from pyparsing import (
     Group, Suppress, Forward, ZeroOrMore, Keyword, ParserElement, StringEnd, ParseException, common,
-    pythonStyleComment, LineEnd
+    pythonStyleComment, LineEnd, Word, alphas, alphanums
 )
 
 import sys
@@ -19,34 +19,45 @@ import time
 def createTextParser() -> ParserElement:
 
     # 
-    # Define Grammar
+    # Define Syntax
     # 
+    
+    # Variable
+    variable_name = Word(alphas, alphanums)
+    intOrVariable = common.integer | variable_name
+    numberOrVariable = common.number | variable_name
+
     # Arguments
     empty_arg = Suppress('()')
-    int_arg = Suppress('(') + common.integer + Suppress(')')
-    number_arg = Suppress('(') + common.number + Suppress(')')
-    coord_args = Suppress('(') + common.number + Suppress(',') + common.number + Suppress(',') + common.number + Suppress(')')
+    intOrVariable_arg = Suppress('(') + intOrVariable + Suppress(')')
+    numberOrVariable_arg = Suppress('(') + numberOrVariable + Suppress(')')
+    coord_args = Suppress('(') + numberOrVariable + Suppress(',') + numberOrVariable + Suppress(',') + numberOrVariable + Suppress(')')
 
     # Commands
     move_abs = Group( Keyword('move_abs') + coord_args )
     move_rel = Group( Keyword('move_rel') + coord_args )
     snap = Group( Keyword('snap') + empty_arg )
-    record_for = Group( Keyword('record_for') + number_arg )
+    record_for = Group( Keyword('record_for') + numberOrVariable_arg )
     start_recording = Group( Keyword('start_recording') + empty_arg )
     stop_recording = Group( Keyword('stop_recording') + empty_arg )
-    wait = Group( Keyword('wait') + number_arg )
+    wait = Group( Keyword('wait') + numberOrVariable_arg )
     comment = pythonStyleComment + LineEnd()
+    varaible_assignment = Group(
+        (variable_name + Suppress('=') + numberOrVariable).setParseAction(
+            lambda t: ["varaible_assignment", t[0], t[1]]
+        )
+    )
 
     # Forward declaration for nested loops
     command = Forward()
 
     # Define loop command with nested commands
-    loop = Group( Keyword("loop") + int_arg + Suppress('{') + Group(ZeroOrMore(command)) + Suppress('}') )
+    loop = Group( Keyword("loop") + intOrVariable_arg + Suppress('{') + Group(ZeroOrMore(command)) + Suppress('}') )
 
     # Finally define command which included loop
     command <<= (
         move_abs | move_rel | snap | record_for | start_recording |
-        stop_recording | wait | comment | loop
+        stop_recording | wait | comment | loop | varaible_assignment
     )
 
     # Top-level parser
@@ -61,8 +72,10 @@ def createTextParser() -> ParserElement:
 def getMacroScript() -> str:
     # Example script
     script_text = """
-    move_abs(1.0, 2.0, 3.0)
-    move_rel(0.5, -0.5, 0)
+    x = 2
+    wait(x)
+    move_abs(1.0,x, 3.0)
+    move_rel(0.5, -0.5, x)
     snap()
     record_for(3.5)
     start_recording()
@@ -70,11 +83,13 @@ def getMacroScript() -> str:
     # My first loop
     loop(5) {
         # My second loop
-        move_abs(1.1, 2.1, 3.1) # My second loop
-        wait(1.0)
+        y = 5
+        move_abs(1.1, x, 3.1) # My second loop
+        wait(y)
         loop(3) {
             snap()
-            wait(0.5)
+            wait(x)
+            wait(y)
         }
     }
     stop_recording()
@@ -83,21 +98,38 @@ def getMacroScript() -> str:
     return script_text
 
 
-def executeCommandList(commandList: List, stage: Stage, camera: Camera) -> None:
+def executeCommandList(commandList: List, stage: Stage, camera: Camera, scopeVariableDict: dict | None = None) -> None:
     
+    if scopeVariableDict is None:
+        scopeVariableDict = {}
+
+    def resolveValue(value: str | int | float) -> int | float:
+        if isinstance(value, str):
+            if value in scopeVariableDict:
+                return scopeVariableDict[value]
+            else:
+                raise Exception(f"The variable '{value}' is undefined.")
+        else:
+            return value
+
+    
+    # Execute the command
+
     for command in commandList:
+
+        print(command)
         
         commandName = command[0]
 
         if commandName == 'move_abs':
 
-            [x, y, z] = command[1:4]
+            [x, y, z] = list(map(resolveValue, command[1:4]))
             print(f'Move absolute for {x, y, z}')
             stage.move_abs((x, y, z), 'um', wait_until_idle= True)
             
         elif commandName == 'move_rel':
 
-            [x, y, z] = command[1:4]
+            [x, y, z] = list(map(resolveValue, command[1:4]))
             print(f'Move relative for {x, y, z}')
             stage.move_rel((x, y, z), 'um', wait_until_idle= True)
             
@@ -112,7 +144,7 @@ def executeCommandList(commandList: List, stage: Stage, camera: Camera) -> None:
             
         elif commandName == 'record_for':
             
-            recordTime = command[1]
+            recordTime = resolveValue(command[1])
             print(f'Record for {recordTime} sec')
             camera.StartGrabbingMax(int(recordTime), pylon.GrabStrategy_OneByOne)
             
@@ -128,19 +160,28 @@ def executeCommandList(commandList: List, stage: Stage, camera: Camera) -> None:
 
         elif commandName == 'wait':
 
-            waitTime = command[1]
+            waitTime = resolveValue(command[1])
             print(f'Wait for {waitTime} sec')
             time.sleep(waitTime)
         
         elif commandName == 'loop':
 
-            numLoop = command[1]
+            # Only support integer value. A float value will be cast and rounded to int. This need to be on the document.
+            numLoop = int(resolveValue(command[1]))
+
             subCommandList = command[2]
 
             for i in range(numLoop):
                 
                 print(f'Loop {i}')
-                executeCommandList(subCommandList, stage, camera)
+                executeCommandList(subCommandList, stage, camera, scopeVariableDict)
+        
+        elif commandName == 'varaible_assignment':
+
+            variable_name = command[1]
+            variable_value = resolveValue(command[2])
+            print(f'Assign {variable_name} = {variable_value}')
+            scopeVariableDict[variable_name] = variable_value
 
 
 if __name__ == '__main__':
